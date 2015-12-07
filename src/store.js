1360fb754c99ff9660fa717a6f79387d8483a4ad
Liu Yang <zation1@gmail.com>
2015-12-07 12:31:34 +0800

    add redux logger

--- src/store.js
+++ src/store.js
@@ -1,23 +1,36 @@
 import thunk from 'redux-thunk';
 import { createStore, applyMiddleware, compose } from 'redux';
 import { reduxReactRouter } from 'redux-router';
-import { createHistory } from 'history';
+import createHistory from 'history/lib/createMemoryHistory';
+import createLogger from 'redux-logger';
 
 import reducers from './reducers';
 import routes from './routes';
 
-let store = compose(
-  applyMiddleware(thunk),
-  reduxReactRouter({
-    routes,
-    createHistory
-  })
-)(createStore)(reducers);
+let store;
 
-if (NODE_ENV !== 'production' && module.hot) {
-  module.hot.accept('./reducers', () => {
-    store.replaceReducer(reducers);
-  });
+if (__DEVELOPMENT__) {
+  store = compose(
+    applyMiddleware(thunk, createLogger()),
+    reduxReactRouter({
+      routes,
+      createHistory
+    })
+  )(createStore)(reducers);
+
+  if (module.hot) {
+    module.hot.accept('./reducers/index', () => {
+      store.replaceReducer(require('./reducers/index'));
+    });
+  }
+} else {
+  store = compose(
+    applyMiddleware(thunk),
+    reduxReactRouter({
+      routes,
+      createHistory
+    })
+  )(createStore)(reducers);
 }
 
 export default store
