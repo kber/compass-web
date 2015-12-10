7bfc9773eb5d210c2464ebb6f28fba91a74c09ae
Liu Yang <zation1@gmail.com>
2015-12-10 15:33:25 +0800

    remove demo code

--- src/containers/profile.js
+++ src/containers/profile.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
 import { Button } from 'react-toolbox/lib/button';
 
-import { updateName } from '../actions/profile';
-
 @connect(
-  state => ({profile: state.profile}),
-  dispatch => bindActionCreators({updateName}, dispatch)
+  state => {
+    return {
+      currentUser: Object.assign({},
+        state.currentUser,
+        state.users.find(user => user.id === state.currentUser.id)
+      )
+    };
+  }
 )
 export default class extends Component {
   render() {
@@ -15,10 +18,7 @@ export default class extends Component {
       <div>
         <h3>Profile Page</h3>
 
-        <p>Name: {this.props.profile.name}</p>
-        <input type="text"
-               defaultValue={this.props.profile.name}
-               onChange={(event) => this.props.updateName(event.target.value)}/>
+        <p>Account Name: {this.props.currentUser.accountName}</p>
       </div>
     );
   }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,13 +1,11 @@
 import { combineReducers } from 'redux';
 import { routerStateReducer } from 'redux-router';
 
-import profile from './profile';
 import users from './users';
 import currentUser from './current-user';
 
 export default combineReducers({
   router: routerStateReducer,
   currentUser,
-  profile,
   users
 });
